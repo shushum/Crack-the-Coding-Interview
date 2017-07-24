b35d90d488a62c5b936b53aba31fed4e7a0419c8
Yegor <zakovryash@gmail.com>
2017-07-24 23:35:13 +0300

    Strings Anagrams is done

---
+++ src/main/java/data/structures/StringsMakingAnagrams.java
@@ -0,0 +1,34 @@
+package data.structures;
+
+import java.util.Scanner;
+
+public class StringsMakingAnagrams {
+    private static final int ALPHABET_CAPACITY = 26;
+    private static final char FIRST_LETTER_CHAR = 'a';
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        String a = in.next();
+        String b = in.next();
+        System.out.println(numberNeeded(a, b));
+    }
+
+    private static int numberNeeded(String first, String second) {
+        int deletions = 0;
+        int[] charDifference = new int[ALPHABET_CAPACITY];
+
+        for (int i = 0; i < first.length(); i++) {
+            charDifference[first.charAt(i) - (int) FIRST_LETTER_CHAR]++;
+        }
+
+        for (int i = 0; i < second.length(); i++) {
+            charDifference[second.charAt(i) - (int) FIRST_LETTER_CHAR]--;
+        }
+
+        for (int i = 0; i < ALPHABET_CAPACITY; i++) {
+            deletions += Math.abs(charDifference[i]);
+        }
+
+        return deletions;
+    }
+}
