ae0e9c54859c3c684958eea0be059e2b648745e9
Yegor <zakovryash@gmail.com>
2017-07-24 21:01:24 +0300

    ArraysLeftRotation is done

--- src/main/java/data/structures/ArraysLeftRotation.java
+++ src/main/java/data/structures/ArraysLeftRotation.java
@@ -1,4 +1,38 @@
 package data.structures;
 
+import java.util.*;
+
 public class ArraysLeftRotation {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        int k = in.nextInt();
+        int a[] = new int[n];
+        for(int a_i=0; a_i < n; a_i++){
+            a[a_i] = in.nextInt();
+        }
+
+        int result[] = leftRotationBy(k, a);
+
+        for (int i = 0; i < n; i++) {
+            System.out.print(result[i] + " ");
+        }
+    }
+
+    private static int[] leftRotationBy(int rotation, int[] rotatedArray){
+        int actualRotation = rotation % rotatedArray.length;
+
+        if (actualRotation == 0){
+            return rotatedArray;
+        } else {
+            int arrayLength = rotatedArray.length;
+            int[] resultArray = new int[rotatedArray.length];
+
+            System.arraycopy(rotatedArray, 0, resultArray, arrayLength - actualRotation, actualRotation);
+            System.arraycopy(rotatedArray, actualRotation, resultArray, 0, arrayLength - actualRotation);
+
+            return resultArray;
+        }
+    }
 }
